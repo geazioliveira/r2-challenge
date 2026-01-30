eb1a28843fe00c3f68057d20278f58ae52a30f1d
Geazi Oliveira <geazioliveira@gmail.com>
2026-01-30 10:54:48 -0300

    Add `stockApiConfig` for managing API keys and base URL

---
+++ src/core/config/stockApi.config.ts
@@ -0,0 +1,9 @@
+export const stockApiConfig = {
+  apiKeys: [
+    '257VAR5ZNM7XEFQ9',
+    'C72ZS24M4T0CJRAP',
+    'SD6SA4FNZM91T33F',
+    'S0B2013SISUG47ED',
+  ],
+  baseURL: 'https://www.alphavantage.co/query',
+}
